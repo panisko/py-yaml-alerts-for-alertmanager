b9d11740239b0a6c9260b8e673645da3d9160460
panisko <65128784+panisko@users.noreply.github.com>
2022-08-02 16:52:21 +0200

    Templating (#2)
    * inital
    
    * added gitignore
    
    * added init files
    
    * updated gitignore
    
    * updated
    
    * Master (#1)
    
    * inital
    
    * added gitignore
    
    * added init files
    
    * updated gitignore
    
    Co-authored-by: Piotr Anisko <piotr@anisko.info>
    
    * added fixes
    
    Co-authored-by: Piotr Anisko <piotr@anisko.info>

--- src/alerts/alerts.py
+++ src/alerts/alerts.py
@@ -1,6 +1,10 @@
+
+import pkgutil
 from pathlib import Path
 from typing import Dict
-from schema import Optional, Schema, SchemaError, Or
+
+from schema import Optional, Schema, SchemaError
+from jinja2 import Template, TemplateError
 
 import yaml
 import logging
@@ -20,116 +24,98 @@ logger = logging.getLogger(__name__)
 logger.addHandler(console_handler)
 logger.setLevel(logging.INFO)
 
-alert_schema = Schema({
-    'name': str,
-    'expr': str,
-    'for': str,
-    'subjectEmail': str,
-})
-
-custom_alert_schema = Schema({
-    'name': str,
-    Optional('expr'): str,
-    Optional('for'): str
-})
-
-config_file_schema = Schema({
-    'alerts': {
-        'defaults': [
-            alert_schema
-        ],
-        'custom_alerts': {
-            'environment': {
-                Or('prd','acc', 'tst', 'sbx'):
-                    [
-                        custom_alert_schema]
-            },
-            'cluster': {
-                "1_tst":
-                    [
-                        custom_alert_schema
-                    ]
-            }
-
-        }
-    }
-
-})
 
 
 class Alerts:
-    YAML_CUSTOM_CONF_PREFIX = 'custom_alerts'
-    YAML_ALERTS_PREFIX = 'alerts'
+    ALERT_SCHEMA = Schema({
+        'alerts':
+            [
+                {'name': str,
+                 'expr': str,
+                 Optional('for'): str,
+                 Optional('severity'): str,
+                 Optional('emailSubject'): str,
+                 Optional('summary'): str
 
-    def __init__(self, path: str = "null"):
-        self.__alert_names = []
-        self.__alerts = []
-        self.__configuration = {}
-        self.__path: Path = Path('')
-        self.__set_file_path(path)
-        content = self.parse_alert_file()
-        self.__configuration = content
+                 }
+            ]
+    })
 
-    def __set_file_path(self, filename: str) -> None:
-        logger.debug("Name of file: " + filename)
-        path = Path(filename)
-        logger.debug("CWD: " + str(path.cwd()))
-        logger.debug("Does file exist: " + str(path.exists()))
-        if path.is_file():
-            self.__path = Path(path)
+    def __init__(self, filename: str = ''):
+        self.__alerts = {}
+        if Path(filename).is_file():
+            self.read_alerts_from_file(filename)
+            self.parse_alerts(self.__alerts)
+
+    @staticmethod
+    def read_file(filename: str) -> yaml:
+        file = Path(filename)
+        try:
+            logger.info("Trying to open file: " + filename)
+            f = open(file=file, encoding="utf-8")
+            content = yaml.load(f, Loader=yaml.SafeLoader)
+            logger.debug("Content :" + str(content))
+            return content
+        except FileNotFoundError as nff:
+            logger.debug("Unable to open file")
+            raise nff
+
+    @staticmethod
+    def parse_alerts(content, schema: Schema = ALERT_SCHEMA) -> Dict:
+        try:
+            schema.validate(content)
+            logger.info("Content validated")
+            return content
+        except SchemaError as se:
+            raise se
+
+    def update_alert(self, position: int, alert: Dict):
+        for key, value in alert.items():
+            self.__alerts['alerts'][position][key] = value
+        logger.debug('after: ' + str(self.__alerts['alerts'][position]))
+
+    def upsert_alert(self, alerts: Dict):
+        alerts_list = []
+        for alert in self.__alerts['alerts']:
+            alerts_list.append(alert['name'])
+
+        for alert in alerts['alerts']:
+            alert_name = alert['name']
+            if alert_name in alerts_list:
+                idx = alerts_list.index(alert_name)
+                logger.debug('Found alert definition, position: ' + str(idx))
+                logger.debug('now: ' + str(self.__alerts['alerts'][idx]))
+                logger.debug('update: ' + str(alert))
+                self.update_alert(position=idx, alert=alert)
+            else:
+                self.__alerts['alerts'].append(alert)
+                alerts_list.append(alert['name'])
+
+    def read_alerts_from_file(self, filename: str, schema: Schema = ALERT_SCHEMA):
+        read_alerts = self.parse_alerts(self.read_file(filename), schema)
+        if len(self.__alerts) == 0:
+            self.__alerts = read_alerts
         else:
-            logger.error("File not found, therefore not set")
-            raise FileNotFoundError
+            self.upsert_alert(alerts=read_alerts)
 
-    def get_file_path(self) -> str:
-        return str(self.__path)
+    def get_alerts(self) -> Dict:
+        return self.__alerts
 
-    def add_alert_def(self, alert: Dict):
-        alerts = {}
-        name = alert['name']
-        expr = alert['expr']
-        alerts['name'] = name
-        alerts['expr'] = expr
-        self.__alert_names.append(name)
-        self.__alerts.append(alerts)
-
-    def len_alerts(self):
-        return len(self.__prep_alerts)
-
-    def parse_alert_file(self) -> Dict:
-        f = open(file=self.__path, encoding="utf-8")
-        content = yaml.load(f, Loader=yaml.SafeLoader)
-        logger.debug("Content :" + str(content))
-        return content
-
-    def get_all_alerts(self):
-        print(self.__alerts)
-
-    def get_custom_configuration(self):
-        return self.__configuration['alerts'][self.YAML_CUSTOM_CONF_PREFIX]
-
-    def get_default_alerts(self):
-        return self.__configuration['alerts']['defaults']
-
-    def get_custom_env_alerts(self, environment=None):
+    def generate_alert_rules(self) -> str:
         try:
-            alerts = self.__configuration['alerts'][self.YAML_CUSTOM_CONF_PREFIX]['environment'][environment]
-        except KeyError:
-            logger.info("Custom alerts for environment: " + environment + " not found")
-            alerts = None
-        return alerts
+            template_file = pkgutil.get_data(__name__, 'templates/alert_rules.j2').decode('utf-8')
+            jinja2_template = Template(template_file)
+            output = jinja2_template.render(self.__alerts)
+            logger.debug('Generated output:')
+            logger.debug(output)
+            return str(output)
+        except TemplateError as error:
+            logger.error("Failed to render template")
+            logger.error(error)
+            raise error
 
-    def get_custom_cluster_alerts(self, cluster=None):
-        try:
-            alerts = self.__configuration['alerts'][self.YAML_CUSTOM_CONF_PREFIX]['cluster'][cluster]
-        except KeyError:
-            logger.info("Custom alerts for cluster: " + cluster + " not found")
-            alerts = None
-        return alerts
-
-    def get_alert(self, name):
-        index = self.__alert_names.index(name)
-        return self.__alerts[index]
-
-    def count_alerts(self) -> int:
-        return len(self.__alerts)
+    def write_alert_rules(self, filename):
+        output = self.generate_alert_rules()
+        f = open(file=filename, encoding='utf-8', mode="w")
+        f.write(output)
+        f.close()
